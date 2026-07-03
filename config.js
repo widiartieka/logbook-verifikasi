91d2615f2cbf04272cff7011b7010235c7a4295c
widiartieka <putriwidia903@yahoo.com>
2026-07-03 14:31:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // GANTI dengan URL Web App hasil Deploy Google Apps Script Anda
 // Contoh: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = "PASTE_URL_WEB_APP_ANDA_DI_SINI";
+const API_URL = "https://script.google.com/macros/s/AKfycbwtT2v2TouH8HAImmdB4ZsqEcD7AAnMxT4-XC_8FDbeHoWULcxqr6d9xCVF-Su6R9Bd0Q/exec";
